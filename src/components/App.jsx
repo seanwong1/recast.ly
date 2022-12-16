c706111279746aef4ee006ce0310da44f38456ff
Sean Wong <sean.uuong@gmail.com>
2022-12-15 19:50:34 -0800

    Complete creating a dynamic Video Player component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
       </div>
       <div className="col-md-5">
         {/* <script src='src/data/exampleVideoData.js'></script> */}
@@ -20,6 +20,7 @@ var App = () => (
 
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
