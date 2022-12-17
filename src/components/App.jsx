6040ca60c25adb8eeeb99f1514dfe343216a9e57
Sean Wong <sean.uuong@gmail.com>
2022-12-17 11:39:11 -0800

    Complete initializing the app with live data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
-const { useState } = React;
+const { useState, useEffect } = React;
 
-var App = () => {
+var App = (props) => {
+  // NEED TO USE isInList to update state for videoList props
   const [videos, isInList] = useState([]);
   const [currentVideo, playVideo] = useState(exampleVideoData[0]);
+  // THIS IS WHERE ERRORS ARE COMING FROM, SHOULD STOP WHEN WE GET DATA TO WORK
+
+  // const acquireVideos = function() {
+  // };
+  useEffect(() => {
+    // re render video list
+  }, [videos, currentVideo]);
+  // acquireVideos();
+  console.log(videos);
 
   return (
     <div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          <div><h5><em>search</em><Search isInList={isInList} /></h5></div>
         </div>
       </nav>
       <div className="row">
@@ -17,8 +27,7 @@ var App = () => {
         </div>
         <div className="col-md-5">
           {/* <script src='src/data/exampleVideoData.js'></script> */}
-
-          <div><h5><em>videoList</em><VideoList videos={exampleVideoData} videoList={videos} playVideo={playVideo}/></h5></div>
+          <div><h5><em>videoList</em><VideoList videos={videos} playVideo={playVideo}/></h5></div>
         </div>
       </div>
     </div>
@@ -28,6 +37,8 @@ var App = () => {
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import searchYouTube from '../lib/searchYouTube.js';
+import Search from './Search.js';
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,23 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+const { useState } = React;
+
+var Search = (props) => {
+  const [inputText, searchText] = useState('');
+
+  return (
+    <div className="search-bar form-inline">
+      <input className="form-control" type="text" value={inputText} onChange={ (event) => { searchText(event.target.value); } } />
+      <button onClick={() => {
+        searchYouTube(inputText, (data) => {
+          props.isInList(data);
+        });
+      }} className="btn hidden-sm-down">
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div>);
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default Search;
+import App from './App.js';
+import searchYouTube from '../lib/searchYouTube.js';
